16fe693b2fb06d5bbddfaa4779cb412781567c04
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2022-02-26 12:06:33 -0800

    Automated Commit

--- index.js
+++ index.js
@@ -1,59 +1,53 @@
-const router = {
-  getEndpoint: async function (currPage, pageName) {
-    var currPageInfo = pageActions.gatherPageInfo(currPage);
-    var pageInfo = pageActions.gatherPageInfo(pageName);
-    var loginPageInfo = pageActions.gatherPageInfo('login');
-    var errorPageInfo = pageActions.gatherPageInfo('404');
-    let userAuth = await routerActions.authPoint();
-
-    if (localStorage.getItem('user') == null) {
-      return {
-        route: 'login',
-        routeInformation: loginPageInfo,
-        sourceRouteInformation: currPageInfo,
-        authentication: userAuth
-      };
-    }
-
-    if (localStorage.getItem('user') != null && userAuth.accessToken == false) {
-      store.logout(1);
-      return {
-        route: 'login',
-        routeInformation: loginPageInfo,
-        sourceRouteInformation: currPageInfo,
-        authentication: userAuth
-      };
-    }
-
-    if (typeof pageInfo == 'undefined') {
-      return {
-        route: '404',
-        routeInformation: errorPageInfo,
-        sourceRouteInformation:
-          typeof currPageInfo != 'undefined' ? currPageInfo : errorPageInfo,
-        authentication: userAuth
-      };
-    }
-
-    if (localStorage.getItem('user') != null && userAuth.accessToken != false) {
-      return {
-        route: pageName,
-        routeInformation: pageInfo,
-        sourceRouteInformation: currPageInfo,
-        authentication: userAuth
-      };
-    }
-  },
-  authPoint: async function () {
-    return await authService(1).then((res) => {
-      return res;
-    });
-  },
-  selective: [''],
-  loadIndex: 1,
-  excludes: ['r', 'login'],
-  function: true,
-  test: 'test completed'
+const authPoint = async function () {
+  return await authService(1).then((res) => {
+    return res;
+  });
 };
 
-export { router };
+const getEndpoint = async function (currPage, pageName) {
+  var currPageInfo = pageActions.gatherPageInfo(currPage);
+  var pageInfo = pageActions.gatherPageInfo(pageName);
+  var loginPageInfo = pageActions.gatherPageInfo("login");
+  var errorPageInfo = pageActions.gatherPageInfo("404");
+  let userAuth = await authPoint();
+
+  if (localStorage.getItem("user") == null) {
+    return {
+      route: "login",
+      routeInformation: loginPageInfo,
+      sourceRouteInformation: currPageInfo,
+      authentication: userAuth,
+    };
+  }
+
+  if (localStorage.getItem("user") != null && userAuth.accessToken == false) {
+    store.logout(1);
+    return {
+      route: "login",
+      routeInformation: loginPageInfo,
+      sourceRouteInformation: currPageInfo,
+      authentication: userAuth,
+    };
+  }
+
+  if (typeof pageInfo == "undefined") {
+    return {
+      route: "404",
+      routeInformation: errorPageInfo,
+      sourceRouteInformation:
+        typeof currPageInfo != "undefined" ? currPageInfo : errorPageInfo,
+      authentication: userAuth,
+    };
+  }
+
+  if (localStorage.getItem("user") != null && userAuth.accessToken != false) {
+    return {
+      route: pageName,
+      routeInformation: pageInfo,
+      sourceRouteInformation: currPageInfo,
+      authentication: userAuth,
+    };
+  }
+};
+
+export { getEndpoint };
