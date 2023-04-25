4e2cf6b3d7b4b302dfdbb5e8255fa3f6bda4f483
cnsdetroit <73463784+cnsdetroit@users.noreply.github.com>
2023-04-24 21:49:30 -0700

    major changes

--- index.js
+++ index.js
@@ -1,62 +1,30 @@
-import { System } from "./class.js";
+import { System } from './class.js';
 
-const system = new System({
-  name: "system-reserved",
-});
+const system = new System({ name: 'system-reserved' });
 
 const getEndpoint = async function (_api, currPage, pageName) {
-  var currPageInfo = _api.gatherPageInfo(currPage);
+  var current = _api.gatherPageInfo(currPage);
   var pageInfo = _api.gatherPageInfo(pageName);
-  var loginPageInfo = _api.gatherPageInfo("login");
-  var errorPageInfo = _api.gatherPageInfo("404");
+  var loginPage = _api.gatherPageInfo('login');
+  var errorPage = _api.gatherPageInfo('404');
+  let excludes = _api.system.getExclusion('public');
   let userAuth = await _api.system.data.authentication(1);
-  let excludes = ["eula", "account_verify", "forgot_password"];
 
-  if (typeof pageInfo == "undefined") {
-    return {
-      route: "404",
-      routeInformation: errorPageInfo,
-      sourceRouteInformation: typeof currPageInfo != "undefined" ? currPageInfo : errorPageInfo,
-      authentication: userAuth,
-    };
-  }
+  let errorCheck = typeof current != 'undefined' ? current : errorPage;
+  let route = { route: pageName, routeInformation: pageInfo, sourceRouteInformation: current, authentication: userAuth };
+  let errorRoute = { route: '404', routeInformation: errorPage, sourceRouteInformation: errorCheck, authentication: userAuth };
+  let loginRoute = { route: 'login', routeInformation: loginPage, sourceRouteInformation: current, authentication: userAuth };
 
-  if (excludes.includes(pageName)) {
-    return {
-      route: pageName,
-      routeInformation: pageInfo,
-      sourceRouteInformation: currPageInfo,
-      authentication: userAuth,
-    };
-  }
+  console.log('router userAuth', userAuth);
 
-  if (localStorage.getItem("user") == null) {
-    return {
-      route: "login",
-      routeInformation: loginPageInfo,
-      sourceRouteInformation: currPageInfo,
-      authentication: userAuth,
-    };
-  }
-
-  if (localStorage.getItem("user") != null && userAuth.accessToken == false) {
+  if (typeof pageInfo == 'undefined') return errorRoute;
+  if (excludes.includes(pageName)) return route;
+  if (localStorage.getItem('user') == null) return loginRoute;
+  if (localStorage.getItem('user') != null && userAuth.accessToken == false) {
     _api.system.logout(1);
-    return {
-      route: "login",
-      routeInformation: loginPageInfo,
-      sourceRouteInformation: currPageInfo,
-      authentication: userAuth,
-    };
+    return loginRoute;
   }
-
-  if (localStorage.getItem("user") != null && userAuth.accessToken != false) {
-    return {
-      route: pageName,
-      routeInformation: pageInfo,
-      sourceRouteInformation: currPageInfo,
-      authentication: userAuth,
-    };
-  }
+  if (localStorage.getItem('user') != null && userAuth.accessToken != false) return route;
 };
 
 export { getEndpoint };
